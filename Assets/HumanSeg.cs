448f44875f3c15a3cfe0457ac43915c9891f4419
fushikky <fushikky@gmail.com>
2019-12-16 03:12:19 +0900

    wip of wip

--- Assets/HumanSeg.cs
+++ Assets/HumanSeg.cs
@@ -9,7 +9,7 @@ public class HumanSeg : MonoBehaviour
     private RenderTexture _captureTexture = null;
     bool initialized = false;
     [SerializeField]
-    GameObject image;
+    Material mat;
 
     [SerializeField]
     ARCameraBackground arCamBg;
@@ -20,13 +20,13 @@ public class HumanSeg : MonoBehaviour
     void Awake()
     {
         _captureTexture = new RenderTexture(Screen.width, Screen.height, 0);
-
     }
 
     void Update()
     {
         // var mat = image.GetComponent<RawImage>().material;
-        var mat = image.GetComponent<Renderer>().material;
+        /// var mat = this.mat.GetComponent<Image>().material;
+        // var mat = image.GetComponent<Renderer>().material;
         // var mat = image.GetComponentInChildren<Renderer>().material;
 
         if (arCamBg.material != null)
@@ -37,16 +37,24 @@ public class HumanSeg : MonoBehaviour
         if (!initialized)
         {
             // stencilTex(256, 192)
-            Debug.LogFormat("stencilTex({0},{1})", stencilTex.width, stencilTex.height);
-            Debug.LogFormat("_captureTexture({0},{1})", _captureTexture.width, _captureTexture.height);
-            float stencilRatio = (float)stencilTex.width / (float)stencilTex.height;
-            float captureRatio = (float)_captureTexture.height / (float)_captureTexture.width;
-            var ratio = captureRatio / stencilRatio;
-            Debug.Log("Debugg ratio:" + ratio);
-            mat.SetFloat("_Ratio", ratio);
-            initialized = true;
+            if (stencilTex != null)
+            {
+                Debug.LogFormat("stencilTex({0},{1})", stencilTex.width, stencilTex.height);
+                Debug.LogFormat("_captureTexture({0},{1})", _captureTexture.width, _captureTexture.height);
+                float stencilRatio = (float)stencilTex.width / (float)stencilTex.height;
+                float captureRatio = (float)_captureTexture.height / (float)_captureTexture.width;
+                var ratio = captureRatio / stencilRatio;
+                Debug.Log("Debugg ratio:" + ratio);
+                mat.SetFloat("_Ratio", ratio);
+                initialized = true;
+            }
         }
         mat.SetTexture("_OverTex", _captureTexture);
         mat.SetTexture("_MaskTex", stencilTex);
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, mat);
     }
 }
