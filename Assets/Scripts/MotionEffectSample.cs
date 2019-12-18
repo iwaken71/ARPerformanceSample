165a03fbcc645f5bcefde07677a38f424be87a43
fushikky <fushikky@gmail.com>
2019-12-18 23:29:24 +0900

    comment script

--- Assets/Scripts/MotionEffectSample.cs
+++ Assets/Scripts/MotionEffectSample.cs
@@ -1,25 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// ターゲットのGameObjectにエフェクトをアタッチするスクリプト
+/// </summary>
 public class MotionEffectSample : MonoBehaviour
 {
+    #region エフェクトをアタッチするターゲット
     [SerializeField]
     GameObject leftHand;
     [SerializeField]
     GameObject rightHand;
+    #endregion
 
+    #region エフェクトのprefab
     [SerializeField]
     GameObject effectPrefab;
     [SerializeField]
     GameObject effect2Prefab;
-
+    #endregion
 
     [SerializeField]
     SkinnedMeshRenderer bodyMesh;
 
     void Start()
     {
+        // エフェクトをInstantiateしGameObjectにアタッチ
         var effect = Instantiate(effectPrefab, Vector3.zero, Quaternion.identity);
         var effect2 = Instantiate(effect2Prefab, Vector3.zero, Quaternion.identity);
         effect.transform.SetParent(leftHand.transform, false);
@@ -28,7 +34,7 @@ public class MotionEffectSample : MonoBehaviour
 
     void Update()
     {
-        // wip flick
+        // MotionCaptureロボットのメッシュのON/OFF
         if (Input.GetMouseButtonDown(0))
         {
             bodyMesh.enabled = !bodyMesh.enabled;
--- Assets/Scripts/OcculusionSample.cs
+++ Assets/Scripts/OcculusionSample.cs
@@ -2,29 +2,47 @@
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 
-[RequireComponent (typeof (Camera))]
-public class OcculusionSample : MonoBehaviour {
+/// <summary>
+/// stencilTextureをshaderにセットし、shader処理結果を画面に出力する
+/// OnRenderImageを使用するため、Cameraコンポーネントが必要(AR Camera)にアタッチされる想定
+/// </summary>
+[RequireComponent(typeof(Camera))]
+public class OcculusionSample : MonoBehaviour
+{
     [SerializeField]
     AROcclusionManager occlusionManager;
 
     [SerializeField]
     Shader shader;
 
+    // debug用のstencilTexture
     [SerializeField]
     Texture2D debugTex;
     Material material;
 
-    void Awake () {
-        material = new Material (shader);
+    void Awake()
+    {
+        material = new Material(shader);
+        Debug.Log("Screen:" + Screen.width + "x" + Screen.height);
     }
-    void OnRenderImage (RenderTexture src, RenderTexture dest) {
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
 #if Unity_Editor
+        // UnityEditor上でのdebug用動作。debugTexをstencilTexとして使う
         material.SetTexture ("_StencilTex", debugTex);
-#else // on device
-        if (occlusionManager != null) {
-            material.SetTexture ("_StencilTex", occlusionManager.humanStencilTexture);
+#else 
+        // 端末上での動作
+        if (occlusionManager != null)
+        {
+            material.SetTexture("_StencilTex", occlusionManager.humanStencilTexture);
+            if (occlusionManager.humanStencilTexture != null)
+            {
+                var tex = occlusionManager.humanStencilTexture;
+                Debug.Log("stencil:" + tex.width + "x" + tex.height);
+
+            }
         }
 #endif
-        Graphics.Blit (src, dest, material);
+        Graphics.Blit(src, dest, material);
     }
 }
