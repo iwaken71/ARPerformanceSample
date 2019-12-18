e007f69aec03b9d5624b01a91027fe545789d89b
fushikky <fushikky@gmail.com>
2019-12-18 23:35:19 +0900

    add MotionCapture sample

---
+++ Assets/Scripts/MotionEffectSample.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// ターゲットのGameObjectにエフェクトをアタッチするスクリプト
+/// </summary>
+public class MotionEffectSample : MonoBehaviour
+{
+    #region エフェクトをアタッチするターゲット
+    [SerializeField]
+    GameObject leftHand;
+    [SerializeField]
+    GameObject rightHand;
+    #endregion
+
+    #region エフェクトのprefab
+    [SerializeField]
+    GameObject effectPrefab;
+    [SerializeField]
+    GameObject effect2Prefab;
+    #endregion
+
+    [SerializeField]
+    SkinnedMeshRenderer bodyMesh;
+
+    void Start()
+    {
+        // エフェクトをInstantiateしGameObjectにアタッチ
+        var effect = Instantiate(effectPrefab, Vector3.zero, Quaternion.identity);
+        var effect2 = Instantiate(effect2Prefab, Vector3.zero, Quaternion.identity);
+        effect.transform.SetParent(leftHand.transform, false);
+        effect2.transform.SetParent(rightHand.transform, false);
+    }
+
+    void Update()
+    {
+        // MotionCaptureロボットのメッシュのON/OFF
+        if (Input.GetMouseButtonDown(0))
+        {
+            bodyMesh.enabled = !bodyMesh.enabled;
+        }
+    }
+}
