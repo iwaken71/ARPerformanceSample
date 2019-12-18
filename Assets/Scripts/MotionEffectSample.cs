d2bb5ff0a3ddfc199fd91b0b71f84b4ee45319ff
fushikky <fushikky@gmail.com>
2019-12-18 15:03:39 +0900

    add Motion Effect Script

---
+++ Assets/Scripts/MotionEffectSample.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionEffectSample : MonoBehaviour
+{
+    [SerializeField]
+    GameObject leftHand;
+
+    [SerializeField]
+    GameObject effectPrefab;
+    [SerializeField]
+    SkinnedMeshRenderer bodyMesh;
+
+    void Start()
+    {
+        var effect = Instantiate(effectPrefab, Vector3.zero, Quaternion.identity);
+        effect.transform.SetParent(leftHand.transform, false);
+    }
+
+    void Update()
+    {
+        // wip flick
+        bodyMesh.enabled = false;
+        bodyMesh.enabled = true;
+    }
+}
